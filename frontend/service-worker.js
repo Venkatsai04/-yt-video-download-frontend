702fb6cade4307ae6794b5c2b9f9cb4f3eac1e00
Venkatsai04 <106298746+Venkatsai04@users.noreply.github.com>
2023-07-21 14:58:45 +0530

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,18 +1,51 @@
+// Define the cache name
+const cacheName = 'yt-downloader-cache-v1';
+
+// List of files to cache
+const filesToCache = [
+  '/',
+  '/main.html',
+  '/main/utits/index.js',
+  '/main/assets/favicon.png',
+  '/path/to/other/assets/asset1.jpg',
+  '/path/to/other/assets/asset2.css',
+  // Add more paths to other assets you want to cache
+];
+
+// Install event - cache files when the service worker is installed
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('yt-downloader-v1').then((cache) => {
-      return cache.addAll([
-        '/main.html',
-        'path/to/other/assets', // Add paths to other assets you want to cache
-      ]);
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
+// Fetch event - serve cached files if available, otherwise fetch from the network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      // Return cached response if available
+      if (response) {
+        return response;
+      }
+      // Fetch from network if not in cache
+      return fetch(event.request);
+    })
+  );
+});
+
+// Activate event - remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
     })
   );
 });
